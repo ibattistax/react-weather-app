04dc07a8ad731f08dd0eeae85a801e388e9b2764
ibattistax <isabel.battista@gmail.com>
2024-06-30 21:50:15 +0100

    Added API integration but node.js crashed so transferring this project to react-weather2

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -1,51 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./CurrentTemperature.css";
 
 export default function CurrentTemperature() {
-  let WeatherData = {
-    date: "Saturday",
-    time: "19:34",
-    description: "sunny",
-    humidity: "75%",
-    windspeed: "1.23 km/h",
-    temperature: 25,
-  };
+  const [weather, setWeather] = useState({ ready: false });
 
-  return (
-    <div className="CurrentTemperature">
-      <div className="weather-app-data">
-        <div>
-          <h1 className="weather-app-city" id="city">
-            London
-          </h1>
-          <p className="weather-app-details">
-            <span id="time">
-              {WeatherData.date} | {WeatherData.time}
-            </span>{" "}
-            | <span id="description">{WeatherData.description}</span>
-            <br />
-            <br />
-            <strong>Humidity </strong>
-            <span id="humidity">{WeatherData.humidity}</span>
-            <br />
-            <strong>Wind</strong>
-            <span id="wind-speed"> {WeatherData.windspeed}</span>
-          </p>
-        </div>
-        <div className="weather-app-temperature">
-          <div id="icon">
-            <img
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
-              class="weather-app-icon"
-              alt=""
-            />
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeather({
+      ready: true,
+      date: "Saturday",
+      time: "19:34",
+      description: response.data.condition.description,
+      precipitation: "--",
+      humidity: response.data.temperature.humidity,
+      windspeed: response.data.wind.speed,
+      temperature: response.data.temperature.current,
+      icon: response.data.condition.icon_url,
+    });
+  }
+
+  function search() {
+    const apiKey = "oaff0b06238ce15e1bda3c4dt5f4a7ba";
+    let city = "London";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weather.ready) {
+    return (
+      <div className="CurrentTemperature">
+        <div className="weather-app-data">
+          <div>
+            <h1 className="weather-app-city" id="city">
+              London
+            </h1>
+            <p className="weather-app-details">
+              <span id="time">
+                {weather.date} | {weather.time}
+              </span>{" "}
+              |{" "}
+              <span id="description" className="text-capitalize">
+                {weather.description}
+              </span>
+              <br />
+              <br />
+              <strong>Humidity </strong>
+              <span id="humidity">{weather.humidity}%</span>
+              <br />
+              <strong>Wind</strong>
+              <span id="wind-speed"> {weather.windspeed} m/s</span>
+            </p>
           </div>
-          <div className="weather-app-temperature-value" id="temperature">
-            {WeatherData.temperature}
+          <div className="weather-app-temperature">
+            <div id="icon">
+              <img src={weather.icon} class="weather-app-icon" alt="" />
+            </div>
+            <div className="weather-app-temperature-value" id="temperature">
+              {Math.round(weather.temperature)}
+            </div>
+            <div className="weather-app-units">ºC | ºF</div>
           </div>
-          <div className="weather-app-units">ºC | ºF</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
